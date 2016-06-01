dc1a9d4a6f3a5844c6c925d3141e3a3b021fa238
root <root@ip-172-31-16-24.eu-west-1.compute.internal>
2016-06-01 12:33:16 +0000

    Committing the playbook to my git repo

---
+++ git.pl
@@ -0,0 +1,25 @@
+
+- hosts: localhost
+  tasks:
+  - name: install git
+    yum: name=git state=latest
+    become: yes
+    become_method: sudo
+
+  - name: clone spring-petclinic
+    git: repo=https://github.com/YitzZu/spring-petclinic.git dest=/home/spring-petclinic
+    become: yes
+    become_method: sudo
+
+  - name: clone course_exercises_devops
+    git: repo=https://github.com/YitzZu/Course_exercises_devops.git dest=/home/course_exercises_devops
+    become: yes
+    become_method: sudo 
+
+  - name: download_maven
+    get_url: url=
+http://apache.spd.co.il/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz dest=/home/course_downloads/maven
+    become: yes
+    become_method: sudo
+
+   
